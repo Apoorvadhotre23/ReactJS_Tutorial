ff01bacf37305a9cfeb89b796b6a7dd269ff5bf6
Apoorvadhotre23 <apoorvadhotre42@gmail.com>
2026-07-27 15:58:29 +0530

    Digital clock

--- Scenario-based-tasks/src/main.jsx
+++ Scenario-based-tasks/src/main.jsx
@@ -2,11 +2,13 @@ import { createRoot } from 'react-dom/client'
 import CheckBox from './Components/HandlingCheckBox/CheckBox.jsx'
 import RadioButton from './Components/HandlingRadioButton/RadioButton.jsx'
 import DropDown from './Components/DropDownMenu/DropDown.jsx'
+import Clock from './Components/DigitalClock/Clock.jsx'
 
 createRoot(document.getElementById('root')).render(
   <>
     <CheckBox/>
     <RadioButton/>
     <DropDown/>
+    <Clock/>
   </>,
 )
