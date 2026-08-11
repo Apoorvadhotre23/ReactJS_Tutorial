83ff18f97586fcba077615d7100e9431d8e570bc
Apoorvadhotre23 <apoorvadhotre42@gmail.com>
2026-08-11 18:59:22 +0530

    Remove and Clear logic is added

--- Redux/src/Redux_Example/Product.jsx
+++ Redux/src/Redux_Example/Product.jsx
@@ -1,4 +1,4 @@
-import { addItem } from "../redux/slice";
+import { addItem , removeItem, clearItems} from "../redux/slice";
 import "./Product.css";
 import {useDispatch} from "react-redux"
 
@@ -34,6 +34,9 @@ function Product() {
         <button className="add-cart-btn" onClick={()=>dispatch(addItem())}>
           Add to Cart
         </button>
+
+        <button className="add-cart-btn remove" onClick={()=>dispatch(removeItem())}>Remove from cart</button>
+        <button className="add-cart-btn clear" onClick={()=>dispatch(clearItems())}>clear cart</button>
 
       </div>
 
--- Redux/src/redux/slice.js
+++ Redux/src/redux/slice.js
@@ -10,9 +10,17 @@ const addToCart = createSlice({
     reducers:{
         addItem:(state)=>{
             state.value += 1;
+        },
+        removeItem:(state)=>{
+            if(state.value > 0) {
+                 state.value -= 1;
+            } 
+        },
+        clearItems:(state)=>{
+            state.value = 0;
         }
     }
 })
 
-export const {addItem} = addToCart.actions;
+export const {addItem, removeItem, clearItems} = addToCart.actions;
 export default addToCart.reducer
